653825b3b7ab02bd0ba54ab1d023cb680751db4a
HaiPhu2702 <phukaht@gmail.com>
2022-09-10 14:09:51 +0700

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
-import cookieParse from "cookie-parser";
 import userRoutes from "./routes/user.js"
 import videoRoutes from "./routes/video.js"
 import commentRoutes from "./routes/comment.js"
